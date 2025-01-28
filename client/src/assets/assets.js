83a2720c31d4d038e6607d08debf1298a9124de0
Sandeep Prajapati <sandeep.prajapati.23.2@mountblue.tech>
2025-01-28 15:28:20 +0530

    added

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -27,6 +27,36 @@ import rbackground from "./remove_background.svg";
 import temp_1 from "./temp_1.jpg";
 import temp_2 from "./temp_2.jpg";
 import tick from "./tick.svg";
+import whiteLogo from "./logo_without_background.png";
+import media_1 from "./media_1.png";
+import media_2 from "./media_2.png";
+import media_3 from "./media_3.png";
+import media_4 from "./media_4.png";
+import media_5 from "./media_5.png";
+import media_6 from "./media_6.png";
+import media_7 from "./media_7.png";
+import media_8 from "./media_8.png";
+import blog_1 from "./blog_1.jpg";
+import blog_2 from "./blog_2.jpg";
+import blog_3 from "./blog_3.jpg";
+import gal_1 from "./gal_1.jpg";
+import gal_2 from "./gal_2.jpg";
+import gal_3 from "./gal_3.jpg";
+import gal_4 from "./gal_4.jpg";
+import gal_5 from "./gal_5.jpg";
+import gal_6 from "./gal_6.jpg";
+import gal_7 from "./gal_7.jpg";
+import gal_8 from "./gal_8.jpg";
+import gal_9 from "./gal_9.jpg";
+import gal_10 from "./gal_10.jpg";
+import gal_11 from "./gal_11.jpg";
+import gal_12 from "./gal_12.jpg";
+import gal_13 from "./gal_13.jpg";
+import gal_14 from "./gal_14.jpg";
+import gal_15 from "./gal_15.jpg";
+import gal_16 from "./gal_16.jpg";
+import gal_17 from "./gal_17.jpg";
+import hifive from "./hifive.svg";
 
 export const assets = {
   logo,
@@ -58,4 +88,34 @@ export const assets = {
   temp_1,
   temp_2,
   tick,
+  whiteLogo,
+  media_1,
+  media_2,
+  media_3,
+  media_4,
+  media_5,
+  media_6,
+  media_7,
+  media_8,
+  blog_1,
+  blog_2,
+  blog_3,
+  gal_1,
+  gal_2,
+  gal_3,
+  gal_4,
+  gal_5,
+  gal_6,
+  gal_7,
+  gal_8,
+  gal_9,
+  gal_10,
+  gal_11,
+  gal_12,
+  gal_13,
+  gal_14,
+  gal_15,
+  gal_16,
+  gal_17,
+  hifive,
 };
